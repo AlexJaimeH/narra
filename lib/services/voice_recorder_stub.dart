399961eaf125bfc14ef722f8e330b4ab2186c8fa
José Alejandro Jaime Huerta <hello.comunamx@gmail.com>
2025-10-01 10:14:06 -0600

    fix(editor): align VoiceRecorder.start signature to onText; wire live transcript updates

--- lib/services/voice_recorder_stub.dart
+++ lib/services/voice_recorder_stub.dart
@@ -1,9 +1,7 @@
-import 'dart:typed_data';
-
-typedef OnChunk = void Function(Uint8List bytes, String mimeType);
+typedef OnText = void Function(String text);
 
 class VoiceRecorder {
-  Future<void> start({OnChunk? onChunk}) async {}
+  Future<void> start({OnText? onText}) async {}
 
   Future<Uint8List?> stop() async { return null; }
 }
